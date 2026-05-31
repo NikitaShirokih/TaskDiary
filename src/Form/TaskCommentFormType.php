1fde6bf45d7e41d208bbd4e8ba1db6dc392fcf9f
NikitaShirokih <scop.nik@gmail.com>
2026-05-31 13:34:03 +0500

    feat(task-comments): добавь форму и вывод комментариев
    Подготовь интерфейс для просмотра и ввода комментариев на странице задачи, чтобы обсуждение было доступно прямо в контексте задачи без перехода на отдельные экраны.
    
    Changes:
    - добавлена форма TaskCommentFormType
    - настроен внешний вид поля ввода комментария
    - добавлен вывод комментариев и формы в шаблон задачи
    
    Affected: src/Form/TaskCommentFormType.php, templates/task/show.html.twig
    Refs: N/A

---
+++ src/Form/TaskCommentFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\TaskComment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class TaskCommentFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('content', TextareaType::class, [
+            'label' => false,
+            'attr' => [
+                'rows' => 4,
+                'placeholder' => 'Введите комментарий к задаче',
+                'class' => 'form-control w-100',
+            ],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => TaskComment::class,
+        ]);
+    }
+}
